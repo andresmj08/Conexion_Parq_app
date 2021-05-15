64780fb309d6158c43b00c686e193b221dc243d0
andresmj08 <andresmj08@gmail.com>
2021-05-14 20:32:33 -0500

    Validation admin register

--- conexion.php
+++ conexion.php
@@ -1,17 +1,27 @@
 <?php
 
-function conectar(){
-    $user_bd = 'root';
-    $password_bd = '';
-    $server_bd = 'localhost';
-    $bd_name = 'parq_app';
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
-    $conexion = mysqli_connect($server_bd , $user_bd , $password_bd , $bd_name) or die ('No es posible Conectarse a la Base de Datos ');
-   //  $conexion = mysqli_connect( 'localhost' , 'root' , '' , 'gps_local');
+    $method = $_SERVER['REQUEST_METHOD'];
+    if($method == "OPTIONS"){
+        die();
+    }
+
+    function conectar(){
+        $user_bd = 'root';
+        $password_bd = '';
+        $server_bd = 'localhost';
+        $bd_name = 'parq_app';
+
+        $conexion = mysqli_connect($server_bd , $user_bd , $password_bd , $bd_name) or die ('No es posible Conectarse a la Base de Datos ');
 
 
 
-    return $conexion;
 
-}
+        return $conexion;
+
+    }
 ?>
